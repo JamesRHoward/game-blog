15394fae59cea5de7df112e7fa1f9ba2aa6fe829
james howard <james@>
2016-05-31 14:52:36 -0700

    added bootstrap folder, css folder, and js folder.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".initally-showing1").toggle();
+    $(".initally-hidden1").toggle();
+    $(".initally-showing2").toggle();
+    $(".initally-hidden2").toggle();
+    $(".initally-showing3").toggle();
+    $(".initally-hidden3").toggle();
+    $(".initally-showing4").toggle();
+    $(".initally-hidden4").toggle();
+    $(".initally-showing5").toggle();
+    $(".initally-hidden5").toggle();
+  });
+});
